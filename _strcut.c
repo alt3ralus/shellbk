60c2bab9872ad7d8e75144c7c3ae696e15509123
alt3ralus <755@holbertonschool.com>
2019-04-10 21:04:22 +0000

    [dev] a function that cuts a string. Return str duplicate

---
+++ _strcut.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+
+/**
+ * _strcut - A function that cuts every word from a sentence.
+ *
+ *
+ *
+ */
+
+char **_strcut(char * line, const char *separator)
+{
+	int count = 0;
+	char *duplicate;
+	char *token;
+	char **retorno;
+
+	duplicate = strdup(line);
+
+	token = strtok(line, separator);
+	count++;
+
+
+	printf("duplicate = %s \n ", duplicate);
+
+	while (token != NULL)
+	{
+		token = strtok(NULL, separator);
+		count++;
+	}
+
+	retorno = malloc(sizeof (char *) * count);
+
+
+	count = 0;
+
+	retorno[count] = strtok(duplicate, separator);
+
+
+	printf("Retorno 1 = %s \n", retorno[count]);
+
+
+	while ( retorno[count] != NULL)
+	{
+		count++;
+		retorno[count] = strtok(NULL, separator);
+		printf("Retorno 2 es %s \n", retorno[count]);
+	}
+
+	return(retorno);
+}
+
+int main(void)
+{
+	char cadena[] = "ls -lat";
+	char **cadenaP;
+	int i = 0;
+
+	cadenaP = _strcut(cadena, " ");
+
+	while(i <= 2)
+	{
+		printf("cadenas main [%d]: %s\n", i, cadenaP[i]);
+		i++;
+	}
+
+	return (0);
+}
